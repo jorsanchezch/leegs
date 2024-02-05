d46f0d7233dd6a2a1ba214e8d2d0648f489de64b
jorsanchezch <jor.sanchezch@gmail.com>
2024-02-05 13:16:56 -0400

    FE: Layout initiated

--- leegs-presentation/src/components/layout.js
+++ leegs-presentation/src/components/layout.js
@@ -1,4 +1,5 @@
 import Nav from './navbar.tsx';
+import BottomNav from './bottom-nav.tsx'; 
 import styles from '../styles/Layout.module.css';
 import { Inter } from 'next/font/google'
 
@@ -7,7 +8,7 @@ const inter = Inter({ subsets: ['latin'] });
 export default function Layout({ children }) {
     return (
     <main className={`${styles.main} ${inter.className}`}>
-        <Nav/>
+        <BottomNav/>
         {children}
     </main>
     );
